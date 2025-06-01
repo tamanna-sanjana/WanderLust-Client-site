cb73a9eb0ef2936445da04822ace14d0f4703d0e
GranPeradisoos <granparadiso729@gmail.com>
2025-06-02 05:23:43 +0600

    Navbar Customization

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from '../Components/Frontend/Navbar';
+import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
-        <div>
+        <>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
             
-        </div>
+        </>
     );
 };
 
