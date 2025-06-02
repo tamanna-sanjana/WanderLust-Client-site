0f9c8567be747cb3033da1edcc6297795723c717
GranPeradisoos <granparadiso729@gmail.com>
2025-06-02 11:22:44 +0600

    Footer

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Frontend/Navbar';
 import { Outlet } from 'react-router';
+import Footer from '../Components/Frontend/Footer';
 
 const Root = () => {
     return (
         <>
         <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
             
         </>
     );
